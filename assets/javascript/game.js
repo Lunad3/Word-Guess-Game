020eb2b2a5ee2832da0e5e1aa5d65ef89887c03e
LunaGram <lunad3@uci.edu>
2018-05-09 13:53:10 -0700

    Logic is working, alph change colr, page functional

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,8 @@
-console.log("in game.js and running")
 
+//---------------------TESTING AREA-------------------------------------------
+
+
+//---------------------------------Game Setup---------------------------------
 var wordLibrary = 
     [
         "hang","rude","shape","average","good","numerous","suffer",
@@ -9,62 +12,81 @@ var wordLibrary =
         "bump","scale","bright","misty","prepare","zealous","hospitable",
         "rightful","spotted","pump","wrap","knife"
     ];
+var usedWords = [];
+var lettersGuessed = [];
+var word_guessed = false;
+var word = wordLibrary[Math.floor(Math.random() * wordLibrary.length)];
+usedWords.push(word);
 
-var alreadySelected = [];
+var wordObj = [];                                                                // 
+for (i=0; i<word.length; i++)                                                    // array of letters and 1/0 : 1 = show, 0 = dont show(default)
+{wordObj.push([0,word[i]]);}                                                     // ex.  [[0,"p"],[1,"e"],[0,"t"]] = "-e-"
+updateWordText();
 
-while (alreadySelected.length < wordLibrary.length)
+
+//----------------------------------  GAME   ----------------------------------
+var lives = 10;
+
+document.onkeyup = function(event)
     {
-        var word = wordLibrary[Math.floor(Math.random() * wordLibrary.length)];          //
-        while (alreadySelected.indexOf(word) != -1)                                      //
-            {                                                                            // select new random word from library
-                var word = wordLibrary[Math.floor(Math.random() * wordLibrary.length)];  //
-            }                                                                            //
-        alreadySelected.push(word);
-
-        var wordObj = [];                                                                //
-        for (i=0; i<word.length; i++)                                                    // array of letters and 1/0 : 1 = show, 0 = dont show(default)
-            {wordObj.push([0,word[i]]);}                                                 // ex.  [[0,"p"],[1,"e"],[0,"t"]] = "-e-"
-        console.log("testing Display Word for :" + word);
-        displayWord();
-        
-        var lettersGuessed = [];
-        var guessesLeft = 10;
-        var word_guessed = false;
-        // while ((guessesLeft > 0) || (word_guessed))                                       // while player has guesses left or has not guessed the word
-        //     {
-        //         document.onkeyup = function(event)                                        // wait for player to press a key
-        //             {
-        //                 var userGuess = event.key;
-        //                 if (lettersGuessed.indexOf(userGuess) == -1)                      // check if that key has already been pressed before
-        //                     {
-        //                         lettersGuessed.push(userGuess);                           // if key not pressed before, add it to list of keys pressed
-        //                         if (word.indexOf(userGuess) != -1)                        // |    if key found within word
-        //                             {                                                     // |    |
-        //                                 for(i=0; i<wordObj.length; i++)                   // |    | go through each letter of word
-        //                                     {                                             // |    |
-        //                                         if (wordObj[i][1] == userGuess)           // |    | find all instances of the letter guessed correctly
-        //                                             {wordObj[i][0] = 1;}                  // |    | and mark the letter as found
-        //                                     }                                             // |    |
-        //                                 displayWord();                                    // |    | update html page to show locations of correctly guessed letter
-        //                             }                                                     // |    |
-        //                         else {guessesLeft--;}                                     // |    =if key not found, subtract a guess
-        //                     }                                                             // |
-        //                 else {console.log("already guessed that letter");}                // =if key pressed before [***FIND OUT WHAT TO DO***]
-        //             }
-        //     }
-        
-        function displayWord()
+        var usrInput = event.key;
+        var usrInputCode = usrInput.charCodeAt(0);
+        if ( 97 <= usrInputCode && usrInputCode <= 122)
             {
-                console.log("in DisplayWORD:");
-                var result = "";
-                for(i=0; i< wordObj.length; i++)
-                    {
-                        if (wordObj[i][0])
-                            {result += wordObj[i][1];}
-                        else
-                            {result += "-";}
-                    }
-                console.log(result);
+                var result = playGuess(usrInput);
+                var msg = "";
+                lives += result;
+                if (result == 1) {msg = "YOU GUESSED CORRECTLY";}
+                else if (result == 0) {msg = "Key has already been pressed";}
+                else {msg = "you guess.... poorly";}
+                document.getElementById("notification").innerHTML = msg;        
             }
+    }
 
+
+//----------------------------------FUNCTIONS----------------------------------
+
+
+function pickNewWord ()                                                             //// pickNewWord reasigns the var word to unused word from library
+    {                                                                               //
+        while (usedWords.indexOf(word) != -1)                                       // while the word has been used before (is in usedWords)
+            {words = wordLibrary[Math.floor(Math.random() * wordLibrary.length)];}  // = pick randoom word from library
+        usedWords.push(word);                                                       // add the word to usedWords (outside of while loop)
+    }
+
+function playGuess (userGuess)                                                      //// Plas takes a char, and returns a bool stating if the 
+    {                                                                               //// guess was correct
+        if (lettersGuessed.indexOf(userGuess) == -1)                                // check if that key has already been pressed before
+            {                                                                       //
+                lettersGuessed.push(userGuess);                                     // if key not pressed before, add it to list of keys pressed
+                if (word.indexOf(userGuess) != -1)                                  // |    if key found within word
+                    {                                                               // |    |
+                        for(i=0; i<wordObj.length; i++)                             // |    | go through each letter of word
+                            {                                                       // |    |
+                                if (wordObj[i][1] == userGuess)                     // |    | find all instances of the letter guessed correctly
+                                    {wordObj[i][0] = 1;}                            // |    | and mark the letter as found
+                            }                                                       // |    |
+                        document.getElementById(userGuess).style.color = "green";   // |    | change letter color to green
+                        updateWordText();                                           // |    | update html page to show locations of correctly guessed letter
+                        return 1;                                                   // |    + return 1 (guessed correctly)
+                    }                                                               // |    |
+                document.getElementById(userGuess).style.color = "red";             // |    | change letter color to red
+                return -1;                                                          // |    |
+            }                                                                       // |    - return -1 (guessed incorrectly)
+        return 0;                                                                   // = return 0 (letter already guessed)
+    }
+
+    
+function updateWordText()
+    {
+        var result = "";
+        for(i=0; i< wordObj.length; i++)
+            {
+                if (wordObj[i][0])
+                    {result += wordObj[i][1];}
+                else
+                    {result += "-";}
+                document.getElementById("word").innerHTML = result;
+            }
+        
     }
